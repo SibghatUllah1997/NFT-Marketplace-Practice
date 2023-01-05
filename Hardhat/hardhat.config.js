b3cea2a5a28c5fc41e028ab2c3b47c9755f7365b
DESKTOP-9P4ND98\AR Computers <syedabdullah009922@gmail.com>
2023-01-05 20:34:44 +0500

    Upgrading the SmartContract Using Hardhat

--- Hardhat/hardhat.config.js
+++ Hardhat/hardhat.config.js
@@ -1,6 +1,55 @@
 require("@nomicfoundation/hardhat-toolbox");
+require('@openzeppelin/hardhat-upgrades');
+
+const privatekey1="3de12ab1e199cd2b0b6a5ff528de1415debb0cfc0b921e5d2b5a78f972fc5785"
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.17",
+  defaultNetwork: "goerli",
+  networks: {
+    hardhat: {
+    },
+    goerli: {
+       
+      url: "https://rpc.ankr.com/eth_goerli",
+      accounts: [privatekey1]
+    },
+    BSCscan: {
+       
+      url: "https://rpc.ankr.com/bsc",
+      accounts: [privatekey1]
+    },
+    Sepolia: {
+       
+      url: "https://rpc.sepolia.org",
+      accounts: [privatekey1]
+    }
+  },
+  solidity: {
+    version: "0.8.17",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200
+      }
+    }
+  },
+  paths: {
+    sources: "./contracts",
+    tests: "./test",
+    cache: "./cache",
+    artifacts: "./artifacts"
+  },
+  mocha: {
+    timeout: 40000
+  },
+  etherscan: {
+    apiKey: "RJ61S4CVJ776QS721JNDPMKXXE6FN9RYIM",
+  },
+  
 };
+// /** @type import('hardhat/config').HardhatUserConfig */
+// module.exports = {
+//   solidity: "0.8.17",
+// };
+
---
+++ Hardhat/scripts/Marketplace.js
@@ -0,0 +1,35 @@
+// We require the Hardhat Runtime Environment explicitly here. This is optional
+// but useful for running the script in a standalone fashion through `node <script>`.
+//
+// You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
+// will compile your contracts, add the Hardhat Runtime Environment's members to the
+// global , and execute the script.
+//const hre = require("hardhat");
+const { ethers, upgrades } = require("hardhat");
+
+
+
+async function main() {
+
+
+    // const lockedAmount = hre.ethers.utils.parseEther("1");
+  // let bookName;
+  // let Author;
+  // let Published;
+  
+    const NFTPractice1 = await hre.ethers.getContractFactory("NFTPractice1");
+    // const NFTPracticedeploy = await NFTPractice.deploy();
+    const proxy = await upgrades.deployProxy(NFTPractice1, [""]);
+    // await NFTPracticedeploy.deployed();
+    await proxy.deployed();
+    // console.log(
+    //   `Lock with 1 ETH and unlock timestamp deployed to ${NFTPractice1.address}`/
+    // );
+  }
+  
+  // We recommend this pattern to be able to use async/await everywhere
+  // and properly handle errors.
+  main().catch((error) => {
+    console.error(error);
+    process.exitCode = 1;
+  });
